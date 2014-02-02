011d7633af49c8d71201e539c4643775b936cd2c
Niall Kavanagh <ntk@bu.edu>
2014-02-02 11:41:19 -0500

    Create instance dir if it doesn't already exist

--- scripts/prepareenv.sh
+++ scripts/prepareenv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 mbta_gtfs_url="http://www.mbta.com/uploadedfiles/MBTA_GTFS.zip"
-root=$(cd -P -- "$(dirname -- "$0")" && printf "%s\n" "$(pwd -P)")
-instance=$(cd $root/../instance; pwd)
-
-echo $instance
+scripts=$(cd -P -- "$(dirname -- "$0")" && printf "%s\n" "$(pwd -P)")
+mkdir -p "$scripts/../instance"
+instance=$(cd $scripts/../instance; pwd)
 
 mkdir -p "$instance/feeds/mbta"
 curl "$mbta_gtfs_url" -o "$instance/feeds/mbta/mbta_gtfs.zip" &&
